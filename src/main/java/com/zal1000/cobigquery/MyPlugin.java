00df63d9a95e4f45f249f7f12c941bef3b6f3ee2
Zalán Horváth <horvathzalan@zal1000.com>
2022-08-04 00:51:01 +0200

    Compiles now
    Signed-off-by: Zalán Horváth <horvathzalan@zal1000.com>

--- src/main/java/com/zal1000/cobigquery/MyPlugin.java
+++ src/main/java/com/zal1000/cobigquery/MyPlugin.java
@@ -1,18 +1,32 @@
 package com.zal1000.cobigquery;
 
-import java.util.logging.Level;
+import net.coreprotect.CoreProtect;
+import net.coreprotect.CoreProtectAPI;
+import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class MyPlugin extends JavaPlugin {
-    // This code is called after the server starts and after the /reload command
-    @Override
-    public void onEnable() {
-        getLogger().log(Level.INFO, "{0}.onEnable()", this.getClass().getName());
-    }
+import java.util.logging.Level;
 
-    // This code is called before the server stops and after the /reload command
-    @Override
-    public void onDisable() {
-        getLogger().log(Level.INFO, "{0}.onDisable()", this.getClass().getName());
-    }
+public class MyPlugin extends JavaPlugin {
+
+
+  // This code is called after the server starts and after the /reload command
+  @Override
+  public void onEnable() {
+    Bukkit.getPluginManager().registerEvents((Listener) this, this);
+    Plugin depend = Bukkit.getPluginManager().getPlugin("CoreProtect");
+    // if (depend == null) {
+    //     getPluginLoader().disablePlugin(this);
+    //     return;
+    // }
+    CoreProtectAPI api = ((CoreProtect) depend).getAPI();
+  }
+
+  // This code is called before the server stops and after the /reload command
+  @Override
+  public void onDisable() {
+    getLogger().log(Level.INFO, "{0}.onDisable()", this.getClass().getName());
+  }
 }
