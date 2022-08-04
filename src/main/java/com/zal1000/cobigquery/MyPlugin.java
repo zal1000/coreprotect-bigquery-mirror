23bda3693f85d4a5cc9e1d58953dbaa54b1eacc1
Zalán Horváth <horvathzalan@zal1000.com>
2022-08-04 02:04:23 +0200

    Maybe working?
    Signed-off-by: Zalán Horváth <horvathzalan@zal1000.com>

--- src/main/java/com/zal1000/cobigquery/MyPlugin.java
+++ src/main/java/com/zal1000/cobigquery/MyPlugin.java
@@ -1,5 +1,6 @@
 package com.zal1000.cobigquery;
 
+import com.google.cloud.bigquery.*;
 import net.coreprotect.CoreProtect;
 import net.coreprotect.CoreProtectAPI;
 import org.bukkit.Bukkit;
@@ -12,17 +13,55 @@ import java.util.logging.Level;
 public class MyPlugin extends JavaPlugin {
 
 
-  // This code is called after the server starts and after the /reload command
-  @Override
-  public void onEnable() {
-    Bukkit.getPluginManager().registerEvents((Listener) this, this);
-    Plugin depend = Bukkit.getPluginManager().getPlugin("CoreProtect");
-    // if (depend == null) {
-    //     getPluginLoader().disablePlugin(this);
-    //     return;
-    // }
-    CoreProtectAPI api = ((CoreProtect) depend).getAPI();
-  }
+    public static void query(String query) {
+        try {
+            // Initialize client that will be used to send requests. This client only needs to be created
+            // once, and can be reused for multiple requests.
+            BigQuery bigquery = BigQueryOptions.getDefaultInstance().getService();
+
+            QueryJobConfiguration queryConfig = QueryJobConfiguration.newBuilder(query).build();
+
+            TableResult results = bigquery.query(queryConfig);
+
+            results
+                    .iterateAll()
+                    .forEach(row -> row.forEach(val -> System.out.printf("%s,", val.toString())));
+
+            System.out.println("Query performed successfully.");
+        } catch (BigQueryException | InterruptedException e) {
+            System.out.println("Query not performed \n" + e);
+        }
+    }
+
+    // This code is called after the server starts and after the /reload command
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents((Listener) this, this);
+        Plugin depend = Bukkit.getPluginManager().getPlugin("CoreProtect");
+        if (depend == null) {
+            getPluginLoader().disablePlugin(this);
+            return;
+        }
+        CoreProtectAPI api = ((CoreProtect) depend).getAPI();
+
+        String projectId = "MY_PROJECT_ID";
+        String datasetName = "MY_DATASET_NAME";
+        String tableName = "MY_TABLE_NAME";
+        String query =
+                "SELECT name, SUM(number) as total_people\n"
+                        + " FROM `"
+                        + projectId
+                        + "."
+                        + datasetName
+                        + "."
+                        + tableName
+                        + "`"
+                        + " WHERE state = 'TX'"
+                        + " GROUP BY name, state"
+                        + " ORDER BY total_people DESC"
+                        + " LIMIT 20";
+        query(query);
+    }
 
   // This code is called before the server stops and after the /reload command
   @Override
